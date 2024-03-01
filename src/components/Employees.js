886d42716b4620ede37b65507452a9ef6c9cd675
ravindu <rcy218@gmail.com>
2024-03-01 13:28:19 +0530

    first commit

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees=[
+    {
+        id:"1",
+        Name:"Ravindu",
+        Age:"29",
+    },
+    {
+        id:"2",
+        Name:"Chamod",
+        Age:"30",
+    },
+    {
+        id:"3",
+        Name:"Yapa",
+        Age:"31",
+    }
+]
+
+export default Employees;
